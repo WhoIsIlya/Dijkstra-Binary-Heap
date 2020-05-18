8131a0f89ca2fc024d1030e67cb05d638ba20da3
WhoIsIlya <worked.mail@mail.ru>
2020-05-18 23:14:39 +0300

    OOP optimise + DijkstraBackward realistion

--- dijkstra_algorithm/Source.cpp
+++ dijkstra_algorithm/Source.cpp
@@ -6,100 +6,126 @@
 #include <set>
 #include <ctime>
 
-using namespace std;
-
 struct Graph {
 public:
 	int numberOfCities;
 	int numberOfRoads;
 	int startPoint;
 	int finishPoint;
-	vector<vector<int>>edges;
+	std::vector<std::vector<int>>edges;
 };
 
-//bool visite[1000000];
+struct Distances
+{
+	std::vector<int> distances;
+	std::vector<int> previous;
+};
 
-void ReadFile(string file);
-
+Graph ReadFile(std::string fileName, Graph inputGraph);
+Distances DijkstraForward(Graph inputGraph);
+std::vector<int> DijkstraBackward(Graph inputGraph, Distances Result);
 
 int main() {
-	int N = 0, M = 0, S = 0, T = 0;
-	ifstream file("input5.txt");
-	if (!file) {
-		cout << "Can't open file";
-		return 0;
-	}
-	else {
-		file >> N >> M >> S >> T;
-		S--;
-		T--;
-		cout << "Params: N=" << N << ", M=" << M << ", S=" << S << ", T=" << T << "." << endl;
+	std::string fileName = "input5.txt";
+	Graph DijkstraGraph = ReadFile(fileName, DijkstraGraph);
+	Distances Result = DijkstraForward(DijkstraGraph);
+	std::cout << "Distance: " << Result.distances[DijkstraGraph.finishPoint] << std::endl;
+	std::vector<int> shortestPath = DijkstraBackward(DijkstraGraph, Result);
 
-		vector<int> visite(1000000);
-		vector<int> dist(1000000);
-		for (int i = 0; i < 1000000; i++) {
-			dist[i] = INT_MAX;
-			visite[i] = false;
-		}
-
-		vector<vector<int>>edges(N, vector<int>(N, 0));
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				edges[i][j] = INT_MAX;
-			}
-		}
-
-		for (int i = 0; i < M; i++) {
-			int a, b, d;
-			file >> a >> b >> d;
-			edges[a - 1][b - 1] = edges[b - 1][a - 1] = d;
-		}
-
-		for (int i = 0; i < 1000000; i++) {
-			visite[i] = false;
-			//dist[i] = INT_MAX;
-		}
-
-		dist[S] = 0;
-		visite[S] = true;
-
-		set<pair<int, int> > q;
-		for (int i = 0; i < N; ++i)
-		{
-			q.insert(make_pair(dist[i], i));
-		}
-
-
-		cout << "Start" << endl;
-		unsigned int start_time = clock();
-		while (!q.empty()) {
-			pair<int, int> cur = *q.begin();
-			q.erase(q.begin());
-
-			for (int i = 0; i < (int)edges[cur.second].size(); ++i) {
-				
-				if (!visite[i] && edges[cur.second][i] != INT_MAX && edges[cur.second][i] + dist[cur.second] < dist[i]) {
-					
-					q.erase(make_pair(dist[i], i));
-
-					int temp = dist[i];
-					if (edges[cur.second][i] != INT_MAX)
-						dist[i] = edges[cur.second][i] + dist[cur.second];
-					if (temp > dist[i]) {
-						q.insert(make_pair(dist[i], i));
-					}
-				}
-			}
-		}
-		unsigned int end_time = clock(); // конечное время
-		unsigned int search_time = end_time - start_time;
-		cout << "runtime = " << search_time / 1000.0 << endl;
-		cout << "Distance: " << dist[T] << ' ';
-	}
 	return 0;
 }
 
-void ReadFile(string File) {
+Graph ReadFile(std::string fileName, Graph inputGraph) {
+	std::ifstream file(fileName);
+	if (!file) {
+		std::cout << "Can't open file";
+	}
+	else {
+		file >> inputGraph.numberOfCities >> inputGraph.numberOfRoads >> inputGraph.startPoint >> inputGraph.finishPoint;
+		inputGraph.startPoint--;
+		inputGraph.finishPoint--;
 
+		inputGraph.edges.resize(inputGraph.numberOfCities, std::vector<int>(inputGraph.numberOfCities, 0));
+
+		for (int i = 0; i < inputGraph.numberOfCities; i++) {
+			for (int j = 0; j < inputGraph.numberOfCities; j++) {
+				inputGraph.edges[i][j] = INT_MAX;
+			}
+		}
+
+		for (int i = 0; i < inputGraph.numberOfRoads; i++) {
+			int edgeStartPoint, edgeEndPoint, edgeWeight;
+			file >> edgeStartPoint >> edgeEndPoint >> edgeWeight;
+			inputGraph.edges[edgeStartPoint - 1][edgeEndPoint - 1] = inputGraph.edges[edgeEndPoint - 1][edgeStartPoint - 1] = edgeWeight;
+		}
+		
+	}
+	return inputGraph;
+}
+
+Distances DijkstraForward(Graph inputGraph) {
+	
+	Distances Result;
+	Result.distances.resize(inputGraph.numberOfCities);
+	Result.previous.resize(inputGraph.numberOfCities);
+	std::vector<int> visite(inputGraph.numberOfCities);
+	
+
+
+	for (int i = 0; i < inputGraph.numberOfCities; i++) {
+		Result.distances[i] = INT_MAX;
+		visite[i] = false;
+	}
+
+	Result.distances[inputGraph.startPoint] = 0;
+	visite[inputGraph.startPoint] = true;
+
+	std::set<std::pair<int, int> > q;
+	for (int i = 0; i < inputGraph.numberOfCities; ++i)
+	{
+		q.insert(std::make_pair(Result.distances[i], i));
+	}
+
+	std::cout << "Start" << std::endl;
+	unsigned int start_time = clock();
+	while (!q.empty()) {
+		std::pair<int, int> cur = *q.begin();
+		q.erase(q.begin());
+
+		for (int i = 0; i < (int)inputGraph.edges[cur.second].size(); ++i) {
+
+			if (!visite[i] && inputGraph.edges[cur.second][i] != INT_MAX && inputGraph.edges[cur.second][i] + Result.distances[cur.second] < Result.distances[i]) {
+
+				q.erase(std::make_pair(Result.distances[i], i));
+
+				int temp = Result.distances[i];
+				if (inputGraph.edges[cur.second][i] != INT_MAX) {
+					Result.distances[i] = inputGraph.edges[cur.second][i] + Result.distances[cur.second];
+					Result.previous[i] = cur.second;
+				}				
+				if (temp > Result.distances[i]) {
+					q.insert(std::make_pair(Result.distances[i], i));
+				}
+			}
+		}
+	}
+	unsigned int end_time = clock(); // конечное время
+	unsigned int search_time = end_time - start_time;
+	std::cout << "runtime = " << search_time / 1000.0 << std::endl;
+	return Result;
+}
+
+std::vector<int> DijkstraBackward(Graph inputGraph, Distances Result) {
+	
+	std::vector<int> path;
+	for (int v = inputGraph.finishPoint; v != inputGraph.startPoint; v = Result.previous[v])
+		path.push_back(v);
+	path.push_back(inputGraph.startPoint);
+
+	reverse(path.begin(), path.end());
+	std::cout << "Path: ";
+	for (int i = 0; i < path.size(); i++)
+		std::cout << path[i] + 1 << ' ';
+	std::cout << std::endl;
+	return path;
 }
